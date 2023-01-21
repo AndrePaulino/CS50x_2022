a1fb5fbafab85e216bfa79f202aab94e8b05cb10
André Paulino <andrepaulinobtw@gmail.com>
2023-01-21 02:29:02 -0300

    lab_09 solution

---
+++ weeks/week_09/lab_09/application.py
@@ -0,0 +1,33 @@
+import os
+
+from cs50 import SQL
+from flask import Flask, flash, jsonify, redirect, render_template, request, session
+
+# Configure application
+app = Flask(__name__)
+
+# Ensure templates are auto-reloaded
+app.config["TEMPLATES_AUTO_RELOAD"] = True
+
+# Configure CS50 Library to use SQLite database
+db = SQL("sqlite:///birthdays.db")
+
+
+@app.route("/", methods=["GET", "POST"])
+def index():
+    if request.method == "POST":
+
+        name = request.form.get("name")
+        month = int(request.form.get("month"))
+        day = int(request.form.get("day"))
+        print(name, month, day)
+        if name and 1 <= month <= 12 and 1 <= day <= 31:
+            db.execute(
+                "INSERT INTO birthdays (name, month, day) VALUES (?, ?, ?)", name, month, day)
+
+        return redirect("/")
+
+    else:
+        people = db.execute("SELECT name, month, day from birthdays")
+
+        return render_template("index.html", people=people)
